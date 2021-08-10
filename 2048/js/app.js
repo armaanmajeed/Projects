6e9d5c3f6c6491a1905105616acead5e7e306ca4
Armaan <armaanmajeed.khan@gmail.com>
2021-08-10 23:03:51 +0530

    Updated

--- 2048/js/app.js
+++ 2048/js/app.js
@@ -3,9 +3,7 @@ const score_display = document.getElementById("score");
 const result_display = document.getElementById("result");
 // create initial board
 const width = 4;
-const squares = Array.from({ length: width }, () =>
-  Array.from({ length: width }, () => 0)
-);
+let squares = [];
 
 document.addEventListener("DOMContentLoaded", () => {
   init();
@@ -14,6 +12,9 @@ document.addEventListener("DOMContentLoaded", () => {
 
 // Initial
 function init() {
+  squares = Array.from({ length: width }, () =>
+    Array.from({ length: width }, () => 0)
+  );
   render_game();
   put_random_value_to_random_place();
   put_random_value_to_random_place();
@@ -86,8 +87,8 @@ function merge_left() {
   for (let i = 0; i < squares.length; i++) {
     let temp = [];
     let length = squares[0].length;
+    let k = 1;
     for (let j = 0; j < squares[i].length - 1; j++) {
-      let k = 1;
       let current = squares[i][j];
       let next = squares[i][j + k];
       while (next === 0) {
@@ -119,12 +120,90 @@ function merge_left() {
   }
 }
 
-function merge_right() {}
+function merge_right() {
+  for (let i = 0; i < squares.length; i++) {
+    let temp = [];
+    let k = 1;
+    for (let j = squares[i].length-1; j >= 0; j--) {
+      let current = squares[i][j];
+      let previous = squares[i][j-k];
+      while (previous === 0) {
+        k++;
+        previous = squares[i][j-k];
+      }
+      if (current === previous) {
+        temp.push(2 * current);
+        squares[i][j] = 0;
+        squares[i][j-k] = 0;
+      } else if (current !== 0) {
+        temp.push(current);
+        squares[i][j] = 0;
+      }
+    }
+    for (let j = squares[i].length-1; j >= 0; j--) {
+      if (squares[i][j] !== 0) {
+        temp.push(squares[i][j]);
+      }
+    }
+    let tempLength = temp.length;
+    let diff = squares[0].length - tempLength;
+    while (diff !== 0) {
+      temp.push(0);
+      diff--;
+    }
+    if (temp.some((e) => e != 0)) {
+      squares[i] = temp.reverse();
+    }
+  }
+}
 
-function merge_up() {}
+function merge_up() {
+  for (let i = 0; i < squares.length; i++) {
+    let temp = [];
+    let k = 1;
+    for (let j = 0; j < squares[i].length; j++) {
+      let current = squares[j][i];
+      console.log("");
+      console.log(squares,j,k,i);
+      console.log(squares[j+k][i]);
+      let under = squares[j+k][i];
+      while (under !== 0 && j+k < squares[i].length) {
+        k++;
+        under = squares[j+k][i];
+      }
+    //   if (current === under) {
+    //     temp.push(2 * current);
+    //     squares[j][i] = 0;
+    //     squares[j+k][i] = 0;
+    //   } else if (current !== 0) {
+    //     temp.push(current);
+    //     squares[j][i] = 0;
+    //   }
+    // }
+    // for (let j = 0; j < squares[i].length; j++) {
+    //   if (squares[j][i] !== 0) {
+    //     temp.push(squares[j][i]);
+    //   }
+    // }
+    // let tempLength = temp.length;
+    // let diff = squares[0].length - tempLength;
+    // console.log(`Diff is ${diff}`);
+    // console.log(`Temp is ${temp}`);
+    // while (diff !== 0) {
+    //   temp.push(0);
+    //   diff--;
+    // }
+    // for (let j = 0; j < squares[i].length; j++) {
+    //   if (temp.some((e) => e != 0)) {
+    //     squares[j][i] = temp[j];
+    //   }
+    }
+  }
+}
 
 function merge_down() {}
 
 function game_over() {
   alert("Game Over");
+  init();
 }
